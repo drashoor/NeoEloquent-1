b87074cd90a429be03da811d489e5adc26612e3e
yassirAwad <com.eng.yassir@gmail.com>
2019-01-26 12:06:42 +0200

    add has one and belong to relations

---
+++ src/Eloquent/Relations/Hybrid/HybridRelations.php
@@ -0,0 +1,45 @@
+<?php namespace Vinelab\NeoEloquent\Eloquent\Relations\Hybrid;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait HybridRelations
+{
+    public function hasOneHybrid($related, $foreignKey = null, $localKey = null)
+    {
+        //TO make relation from non-relational to relational
+        if (!is_subclass_of($related, 'Vinelab\NeoEloquent\Eloquent\Model')) {
+            return Model::hasOne($related, $foreignKey, $localKey);
+        }
+
+        //TO make relation from relational to non-relational
+        $relation = $this->guessBelongsToRelation();
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+        $localKey = $localKey ?: $this->getKeyName();
+        $instance = $this->newRelatedInstance($related);
+
+        return new HasOne($instance->newQuery(), $this, $foreignKey, $localKey, $relation);
+    }
+
+    public function belongsToHybrid($related, $foreignKey = null, $ownerKey = null, $relation = null)
+    {
+        if (is_null($relation)) {
+            $relation = $this->guessBelongsToRelation();
+        }
+
+        //TO make relation from non-relational to relational
+        if (!is_subclass_of($related, 'Vinelab\NeoEloquent\Eloquent\Model')) {
+            return Model::belongsTo($related, $foreignKey, $ownerKey, $relation);
+        }
+
+        //TO make relation from relational to non-relational
+
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+        $ownerKey = $ownerKey ?: $this->getKeyName();
+        $instance = $this->newRelatedInstance($related);
+        $table = last($instance->getTable());
+
+        return $this->newBelongsTo(
+            $instance->newQuery(), $this, "$table.$foreignKey", $ownerKey, $relation
+        );
+    }
+}
